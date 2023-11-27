6830935c74bfa3b467e6a4bf009e2b342ac112df
sreeshanath <sreesha.s.nath@gmail.com>
2023-11-27 13:36:18 -0700

    added database connection

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var app = express();
+var pgp = require('pg-promise')();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+const PORT = 80;
+
+const dbConfig = {
+	host: process.env.host,
+	port: 5432,
+	database: process.env.database,
+	user: process.env.user,
+	password: process.env.password
+};
+
+const db = pgp(dbConfig);
+
 
 app.get('/', function(req, res){
 	res.send('<h1>Working</h1>');
 });
+
+app.get('/dbtest', (req,res)=>{
+	var query = `SELECT * FROM users;`;
+	db.any(query)
+	.then( data =>{
+		res.send(data);
+	})
+	.catch(err => {
+		res.send(err);
+	})
+})
 
 console.log(process.env.PORT);
 
