664193d60e554ab68eca6b5085a39f1ab477758c
sreeshanath <sreesha.s.nath@gmail.com>
2023-11-28 21:36:01 -0700

    merged with remote

---
+++ data/insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO users (username, password, email) VALUES
+('chpa1253','f203295c012b14c580302f4fae101817ec085949','chloe.parsons@example.com'),
+('owbl0483','c580302f4fa12bf203295c014e101817ec085949','owen.blake@example.com'),
+('joba0218','580302f4fae112bf203295c014c01817ec085949','joshua.bailey@example.com'),
+('lami7364','12bf203295c014c580302f4fae101817ec085949','lauren.mitchell@example.com'),
+('gasa4729','302f4fae1018112bf203295c014c5807ec085949','grace.sanderson@example.com'),
+('saba3467','1817ec08594912bf203295c014c580302f4fae10','samantha.ball@example.com');
